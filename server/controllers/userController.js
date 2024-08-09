dbf1d7e13fd4db7fb1212ab3f6681cd763e647f2
Skyler Jones <skylerrhys@gmail.com>
2024-08-08 19:28:01 -0500

    added login and Auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,7 @@
 const { User } = require('../models');
 
+const { signToken } = require('../utils/auth.js');
+
 module.exports = {
     async getSingleUser(req, res) {
         try {
@@ -14,6 +16,26 @@ module.exports = {
         try {
             const data = await User.create(req.body);
             res.json(data);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    async login(req, res) {
+        try {
+            const data = await User.findOne({ $or: [{ username: req.body.username }, { email: req.body.email }] });
+
+            if (!data) {
+                res.status(400).json({ message: 'Incorrect email or password, please try again.'});
+            }
+
+            const validPassword = await data.isCorrectPassword(req.body.password);
+
+            if (!validPassword) {
+                return res.status(400).json({ message: 'Wrong password!' });
+            }
+            const token = signToken(user);
+            res.json({ token, data });
         } catch (err) {
             res.status(500).json(err);
         }
