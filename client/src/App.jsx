dee427221d3fb76249aefad014c02d26700192a8
Giovanni Ramirez <jesussavedgr78@gmail.com>
2024-08-12 19:43:56 -0400

    fixing main.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import { useState } from 'react'
-import Auth from './pages/auth'
+// import { useState } from 'react'
+// import Auth from './pages/auth'
 // import './App.css'
 import Navbar from './components/Navbar';
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,31 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App.jsx'
+import Error from './pages/error.jsx';
+import LandingPage from './pages/landingPage.jsx';
+import DashBoard from './pages/dashBoard.jsx';
+
+
+//adding a comment here to fix the main jsx
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <LandingPage />,
+      },
+      {
+        path: '/DashBoard',
+        element: <DashBoard />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-)
+    <RouterProvider router={router} />
+);
