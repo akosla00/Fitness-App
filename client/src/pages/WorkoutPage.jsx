68fd5479c3b9dca12b8050c96e669970bfaec44c
kristyyip <kristyyip51998@gmail.com>
2024-08-19 14:58:57 -0400

    changed exercise _id to autoincremented number for seeding purposes

--- client/src/pages/WorkoutPage.jsx
+++ client/src/pages/WorkoutPage.jsx
@@ -1,10 +1,13 @@
-import AddWorkout from '../components/workoutPage/AddWorkout.jsx'
-import WorkoutPlans from '../components/workoutPage/WorkoutPlans.jsx'
+import  { Typography } from '@mui/material';
+
+import AddWorkout from '../components/workoutPage/AddWorkout.jsx';
+import WorkoutPlans from '../components/workoutPage/WorkoutPlans.jsx';
 
 
 function WorkoutPage() {
     return (
         <>
+            <Typography variant='h3' align='center'>Workouts</Typography>
             <AddWorkout />
             <WorkoutPlans />
         </>
